d4c528e249ef0aa223319fc266f28539afeca275
SAT <novikov.andrey.v@hotmail.com>
2020-06-15 15:46:06 +0300

    Refactoring ConnectionString

--- Test/Startup.cs
+++ Test/Startup.cs
@@ -22,7 +22,8 @@ namespace Test
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            string connectionString = @"Server=SAT-GL\SQLEXPRESS01;Database=TestExercise1;Trusted_Connection=True;";
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+            //string connectionString = @"Server=SAT-GL\SQLEXPRESS01;Database=TestExercise1;Trusted_Connection=True;";
             services.AddDbContext<ApplicationContext>(option => option.UseSqlServer(connectionString));
             services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
